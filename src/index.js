487134200fcdf458e3897652eb5f2fac6886ac20
Arazal <arazaladle@gmail.com>
2019-04-02 15:24:52 +0300

    redux middleware

--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux'
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
+import { createLogger } from 'redux-logger'
 import './index.css';
 import App from './containers/App'
 import { searchRobots } from './redusers'
 import 'tachyons'
 
 
+const logger = createLogger()
 
-
-const store = createStore(searchRobots)
+const store = createStore(searchRobots, applyMiddleware(logger))
 
 ReactDOM.render(
     <Provider store={store}>
