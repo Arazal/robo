4da99d3e60f52430b1801d4e1926e7cd76aca5cf
Arazal <arazaladle@gmail.com>
2019-04-02 18:03:00 +0300

    redux finilized

--- src/containers/App.js
+++ src/containers/App.js
@@ -5,42 +5,48 @@ import SearchBox from '../components/SearchBox/SearchBox'
 import Scroll from '../components/Scroll/Scroll'
 import ErrorBoundry from '../components/ErrorBoundry/ErrorBoundry'
 
-import { setSearchField } from '../actions'
+import { setSearchField, requestRobots } from '../actions'
 
 const mapStateToProps = state => {
     return {
-        searchField: state.searchField
+        searchField: state.searchRobots.searchField,
+        robots: state.requestRobots.robots,
+        isPending: state.requestRobots.isPending,
+        eroor: state.requestRobots.error
     }
 }
 
 const mapDispatchToProps = (dispatch) => {
     return {
-        onSearchChange: (event) => dispatch(setSearchField(event.target.value))
+        onSearchChange: (event) => dispatch(setSearchField(event.target.value)),
+        onRequestRobots: () => dispatch(requestRobots())
+
     }
 }
 
 class App  extends Component {
 
-    constructor() {
-        super()
-        this.state = {
-            robots: [],
+    // constructor() {
+    //     super()
+    //     this.state = {
+    //         robots: [],
            
-        }
-    }
+    //     }
+    // }
 
     componentDidMount() {
+        this.props.onRequestRobots()
        
-        fetch('https://jsonplaceholder.typicode.com/users')
-            .then(responce => responce.json())
-            .then(results => this.setState({robots: results}))
+        // fetch('https://jsonplaceholder.typicode.com/users')
+        //     .then(responce => responce.json())
+        //     .then(results => this.setState({robots: results}))
     }
 
   
 
     render() {
-        const {robots } = this.state
-        const { searchField, onSearchChange}  = this.props
+         //const {robots } = this.state
+        const { searchField, onSearchChange, robots, isPending}  = this.props
         const filteredRobots = robots.filter(
             robot => {
                 return robot.name.toLowerCase().includes(
@@ -48,7 +54,7 @@ class App  extends Component {
                 )
             }
         )
-        return robots.lenght ?
+        return isPending ?
              <h1>Loading</h1> :
         (
                 <div>
